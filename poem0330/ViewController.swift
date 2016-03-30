845eed74842bf15ad9ac8038739e526be2675786
我 <Wen-Lin@wenlintsaidemacbook-pro.local>
2016-03-30 11:48:54 +0800

    Initial Commit

---
+++ poem0330/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poem0330
+//
+//  Created by WenLin on 2016/3/30.
+//  Copyright © 2016年 WenLin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ poem0330Tests/poem0330Tests.swift
@@ -0,0 +1,36 @@
+//
+//  poem0330Tests.swift
+//  poem0330Tests
+//
+//  Created by WenLin on 2016/3/30.
+//  Copyright © 2016年 WenLin. All rights reserved.
+//
+
+import XCTest
+@testable import poem0330
+
+class poem0330Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
